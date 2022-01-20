7f0f8ec2d288262c85d54cd70393eccbb096c8a5
gydis <egor.eremin@aalto.fi>
2022-01-20 16:08:03 +0200

    Ex. 5.6, 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,41 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from "react";
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
 
-export default Blog
+  const [hidden, setHidden] = useState(false);
+  const hideWhenVisible = { display: hidden ? "none" : "" };
+  const showWhenVisible = { display: hidden ? "" : "none" };
+
+  const hideBlogStyle = { ...blogStyle, ...hideWhenVisible };
+  const showBlogStyle = { ...blogStyle, ...showWhenVisible };
+
+  const toggleVisibility = () => {
+    setHidden(!hidden);
+  };
+
+  return (
+    <div>
+      <div style={hideBlogStyle}>
+        {blog.title} {blog.author}{" "}
+        <button onClick={toggleVisibility}>view</button>
+      </div>
+      <div style={showBlogStyle}>
+        {blog.title} {blog.author}{" "}
+        <button onClick={toggleVisibility}>hide</button> <br />
+        {blog.url} <br />
+        likes {blog.likes} <button>like</button>
+        <br />
+        {blog.user.name} <br />
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
+
