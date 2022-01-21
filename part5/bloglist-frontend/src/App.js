f0f9d6014be5f22c01f96a7085705f43f0ba9c64
gydis <egor.eremin@aalto.fi>
2022-01-21 20:28:33 +0200

    eslint autofix

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,51 +1,51 @@
-import React, { useState, useEffect } from "react";
-import Blogs from "./components/Blogs";
-import blogService from "./services/blogService";
-import LoginForm from "./components/LoginForm";
-import loginService from "./services/loginService";
-import Notification from "./components/Notification";
+import React, { useState, useEffect } from 'react'
+import Blogs from './components/Blogs'
+import blogService from './services/blogService'
+import LoginForm from './components/LoginForm'
+import loginService from './services/loginService'
+import Notification from './components/Notification'
 
 const App = () => {
-  const [blogs, setBlogs] = useState([]);
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [user, setUser] = useState(null);
-  const [message, setMessage] = useState(null);
-  const [error, setError] = useState(false);
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+  const [message, setMessage] = useState(null)
+  const [error, setError] = useState(false)
 
   useEffect(() => {
-    blogService.getAll().then((blogs) => setBlogs(blogs));
-  }, []);
+    blogService.getAll().then((blogs) => setBlogs(blogs))
+  }, [])
 
   useEffect(() => {
-    const loggedUser = window.localStorage.getItem("loggedUser");
+    const loggedUser = window.localStorage.getItem('loggedUser')
     if (loggedUser) {
-      const newUser = JSON.parse(loggedUser);
-      setUser(newUser);
-      blogService.setToken(newUser.token);
+      const newUser = JSON.parse(loggedUser)
+      setUser(newUser)
+      blogService.setToken(newUser.token)
     }
-  }, []);
+  }, [])
 
   const handleLogin = async (event) => {
-    event.preventDefault();
+    event.preventDefault()
 
     try {
-      const user = await loginService.login({ username, password });
-      setUser(user);
-      window.localStorage.setItem("loggedUser", JSON.stringify(user));
-      setUsername("");
-      setPassword("");
-      blogService.setToken(user.token);
+      const user = await loginService.login({ username, password })
+      setUser(user)
+      window.localStorage.setItem('loggedUser', JSON.stringify(user))
+      setUsername('')
+      setPassword('')
+      blogService.setToken(user.token)
     } catch (exception) {
-      console.log(exception);
-      setMessage("Wrong username or password");
-      setError(true);
+      console.log(exception)
+      setMessage('Wrong username or password')
+      setError(true)
       setTimeout(() => {
-        setMessage(null);
-        setError(false);
-      }, 5000);
+        setMessage(null)
+        setError(false)
+      }, 5000)
     }
-  };
+  }
 
   return (
     <div>
@@ -71,7 +71,7 @@ const App = () => {
         </div>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
